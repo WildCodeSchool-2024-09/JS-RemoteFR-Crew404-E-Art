a770e44ca96a27b8343040f6a02f85f45722ceee
Anthony Gorski <anthony.developpeurweb@gmail.com>
2025-01-10 14:59:23 +0100

    add menu burger

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,31 +1,18 @@
+import { CircleUserRound } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo_art from "../../assets/images/logo_art.png";
-import menu from "../../assets/images/menu.png";
-import "./NavBar.css";
-import { CircleUserRound } from "lucide-react";
 import Button from "../Button/Button";
+import MenuBurger from "../Menu/MenuBurger";
+import "./NavBar.css";
 
 function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
     <nav className="navbar">
       <img className="logo" src={logo_art} alt="La tête d'un objet d'art" />
-      <div className="navbar_header">
-        <button
-          className="burger-menu"
-          type="button"
-          onClick={toggleMenu}
-          aria-label="Toggle menu"
-        >
-          <img src={menu} alt="Menu burger" className="menu_icon" />
-        </button>
-      </div>
+      <MenuBurger />
       <ul className={`navbar-links ${isMenuOpen ? "open" : ""}`}>
         <li>
           <Link to="/" onClick={() => setIsMenuOpen(false)}>
