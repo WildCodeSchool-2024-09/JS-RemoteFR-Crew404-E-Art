c0669108513c00397a92287f4183b9af9ba26f78
Anthony Gorski <anthony.developpeurweb@gmail.com>
2025-01-10 10:25:55 +0100

    header footer and main content done

--- client/src/pages/Contact/Contact.tsx
+++ client/src/pages/Contact/Contact.tsx
@@ -1,75 +1,53 @@
-import { useState } from "react";
-
-import "./Contact.css";
 import Button from "../../components/Button/Button";
+import "./Contact.css";
 
 function Contact() {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    setTimeout(() => {
-      setIsSubmitting(false);
-      setIsSubmitted(true);
-
-      setTimeout(() => {
-        setIsSubmitted(false);
-      }, 2000);
-    }, 2000);
-  };
-
   return (
     <section>
       <h1>Contact me !</h1>
-      {isSubmitted ? (
-        <p>Merci ! Votre message a été envoyé.</p>
-      ) : (
-        <form className="form" onSubmit={handleSubmit}>
-          <div>
-            <label className="contact_form" htmlFor="name">
-              Votre nom
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Ali baba"
-              required
-            />
-          </div>
 
-          <div>
-            <label className="contact_form" htmlFor="email">
-              Votre email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="ali@yahoo.com"
-              required
-            />
-          </div>
+      <form className="form">
+        <div>
+          <label className="contact_form" htmlFor="name">
+            Votre nom
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Ali baba"
+            required
+          />
+        </div>
 
-          <div>
-            <label className="contact_form" htmlFor="message...">
-              Votre message
-            </label>
-            <textarea
-              name="message"
-              id="message"
-              rows={7}
-              placeholder="Votre message"
-              required
-            />
-          </div>
+        <div>
+          <label className="contact_form" htmlFor="email">
+            Votre email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="ali@yahoo.com"
+            required
+          />
+        </div>
 
-          <Button name="Envoyer" />
-        </form>
-      )}
+        <div>
+          <label className="contact_form" htmlFor="message...">
+            Votre message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            rows={7}
+            placeholder="Votre message"
+            required
+          />
+        </div>
+
+        <Button name="Envoyer" />
+      </form>
     </section>
   );
 }
