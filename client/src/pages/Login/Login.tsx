e502b9869bb3737c7ff4d5526933b0f0936956e2
Frank Sena AGBOLOSU <francksena@yahoo.com>
2025-01-15 18:12:50 +0100

    html for login component and login.css

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,10 +1,47 @@
+import Button from "../../components/Button/Button";
 import "./Login.css";
 
 function Login() {
   return (
     <div className="login">
       <section>
-        <h1>Login</h1>
+        <h1 className="login_title">Login</h1>
+        <form className="login_form">
+          <div>
+            <label className="label_login" htmlFor="email">
+              Email
+            </label>
+            <br />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="john@gmail.com"
+              required
+              className="login_input"
+            />
+          </div>
+          <br />
+          <div>
+            <label className="label_login" htmlFor="password">
+              Password
+            </label>
+            <br />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="*********"
+              required
+              className="login_input"
+            />
+          </div>
+          <br />
+
+          <div className="login_button">
+            <Button name="login" />
+          </div>
+        </form>
       </section>
     </div>
   );
