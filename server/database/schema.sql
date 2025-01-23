ece74cd0be0f995d5f13a4112af043a04724f34c
Frank Sena AGBOLOSU <francksena@yahoo.com>
2025-01-23 09:21:37 +0100

    la mise en place de schema.sql

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,21 +1,49 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+CREATE TABLE role (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR (255) NOT NULL
+);
+CREATE TABLE user (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+
+  role_id INT UNSIGNED NOT NULL,
+  CONSTRAINT fk_user_role
+  FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+CREATE TABLE oeuvre (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  image VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  dimension VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  year DATE NOT NULL,
+  medium VARCHAR(255) NOT NULL,
+
+  user_id INT UNSIGNED NOT NULL,
+  CONSTRAINT fk_oeuvre_user
+  FOREIGN KEY(user_id) REFERENCES user(id)
 );
 
-insert into user(id, email, password)
-values
-  (1, "jdoe@mail.com", "123456");
 
-insert into item(id, title, user_id)
+
+CREATE TABLE user_oeuvre (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  oeuvre_id INT UNSIGNED NOT NULL,
+  CONSTRAINT fk_user_oeuvre
+  FOREIGN KEY (user_id) REFERENCES user(id),
+  FOREIGN KEY (oeuvre_id) REFERENCES oeuvre(id)
+);
+INSERT INTO role(id, name) VALUES
+  (1, "admin"),
+  (2, "user");
+insert into user(id, name, email, password, role_id)
 values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
+  (1, "anthou", "anthou@yahoo.com", "word",1);
+
+  
+
+
